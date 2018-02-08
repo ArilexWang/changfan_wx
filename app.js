ff69ca0892c24242b97d0db842f2d86caf9b4877
ArilexWang <383234436@qq.com>
2018-02-08 15:16:16 +0800

    user and recycle

--- app.js
+++ app.js
@@ -8,13 +8,7 @@ App({
     var that = this
     wx.getSystemInfo({
       success: function (res) {
-        console.log(res.model)
-        console.log(res.pixelRatio)
-        console.log(res.windowWidth)
-        console.log(res.windowHeight)
-        console.log(res.language)
-        console.log(res.version)
-        console.log(res.platform)
+        
         that.globalData.model = res.model
       }
     })
---
+++ pages/recycle/recycle.js
@@ -0,0 +1,90 @@
+// pages/recycle/recycle.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banner:{
+      img:"http://p3pu3eqqb.bkt.clouddn.com/%E5%B1%8F%E5%B9%95%E5%BF%AB%E7%85%A7%202018-02-08%20%E4%B8%8B%E5%8D%881.16.03.png"
+    },
+    hot_types: [
+      {
+        model:'iPhone6',
+        image: 'http://p3pu3eqqb.bkt.clouddn.com/iphone.png',
+        hightest_price: 1750,
+      },
+      {
+        model: 'iPhone6',
+        image: 'http://p3pu3eqqb.bkt.clouddn.com/iphone.png',
+        hightest_price: 1750,
+      },
+      {
+        model: 'iPhone6',
+        image: 'http://p3pu3eqqb.bkt.clouddn.com/iphone.png',
+        heightest_price: 1750,
+      },
+      {
+        model: 'iPhone6',
+        image: 'http://p3pu3eqqb.bkt.clouddn.com/iphone.png',
+        hightest_price: 1750,
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
